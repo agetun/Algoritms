7e29d90536093f99aebba45ccf4866c428e8507b
Sasha.Getun <sashagetun@gmail.com>
2023-05-01 12:15:43 +0200

    Homework_6

---
+++ Homeworks/src/homework_6/Main.java
@@ -0,0 +1,74 @@
+package homework_6;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+//Дан массив целых чисел. Числа в массиве расположены в произвольном порядке (массив не отсортирован).
+// Необходимо выбрать опорный элемент (любым способом - первый элемент или элемент, расположенный посередине массива).
+// Необходимо преобразовать массив таким образом, чтобы элементы, меньшие опорного оказались в массиве левее опорного,
+// а большие - правее. Замечание: после работы алгоритма, вы должны знать, где располагается опорный элемент
+// (ведь в результате работы алгоритма он возможно переместится со своего первоначального расположения).
+public class Main {
+
+    public static void main(String[] args) {
+        List<Integer> list = qsort(List.of(12, 10, 7, 2, 4, 6, 9, 15));
+        System.out.println(list);
+    }
+
+
+    private static void qsort(int[] array, int low, int high) {
+        if (array.length < 2 || low >= high) {
+            return;
+        }
+        // Опорный элемент
+        int pivot = array[low + (high - low) / 2];
+        int i = low, j = high;
+        while (i <= j) {
+            while (array[i] < pivot) {
+                i++;
+            }
+            while (array[j] > pivot) {
+                j--;
+            }
+            if (i <= j) {
+                // меняем местами i-ый элемент и j-ый
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                i++;
+                j--;
+            }
+        }
+        // Рекурсия для сортировки левой и правой части
+
+    }
+
+    // TODO Разобраться и исправить ошибку
+    private static List<Integer> qsort(List<Integer> array) {
+        // Базовый случай
+        if (array.size() < 2) {
+            return array;
+        }
+        // Опорный элемент
+        int pivot = array.get(0);
+        // Массив элементов, меньших pivot
+        ArrayList<Integer> less = new ArrayList<>();
+        // Массив элементов, больших pivot
+        ArrayList<Integer> greater = new ArrayList<>();
+        for (int i = 0; i < array.size(); i++) {
+            if (array.get(i) < pivot) {
+                less.add(array.get(i));
+            } else {
+                greater.add(array.get(i));
+            }
+        }
+        ArrayList<Integer> result = new ArrayList<>();
+        result.addAll(qsort(less));
+//        result.add(pivot);
+        result.addAll(qsort(greater));
+        return result;
+    }
+
+}
